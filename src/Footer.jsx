cf1305dc28d132039b8e4e925ced540b1b146563
sahajat-2012 <sahajat.alam2012@gmail.com>
2024-11-22 23:55:50 +0900

    mlodified

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,7 +2,7 @@
 function Footer(){
     return(
         <footer>
-            <p>&copy; {new Date().getFullYear()} Your website name</p>
+            <p>&copy; {new Date().getFullYear()} Your website name gfhgfhg</p>
         </footer>
     );
 }
